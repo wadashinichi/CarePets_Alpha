04d44a753368515f537e97cb33e3966fdeb2de07
wadashinichi <tienkddd@gmail.com>
2023-04-07 12:46:24 +0700

    Start 2 Fragment

--- app/src/main/java/com/example/carepets_alpha/MainActivity.kt
+++ app/src/main/java/com/example/carepets_alpha/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.carepets_alpha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carepets_alpha.database.Pet
 import com.example.carepets_alpha.petlist.PetListAdapter
+import com.example.carepets_alpha.petlist.PetListFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        replaceFragment(PetListFragment())
+    }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+//        fragmentTransaction.commit()
     }
 }
